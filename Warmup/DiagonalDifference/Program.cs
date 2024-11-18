af3d33648ac6df159514c24c3a440550c859e7de
WillMarciano <willian_marciano@hotmail.com>
2024-11-18 16:34:48 -0300

    add Diagonal Difference

---
+++ Warmup/DiagonalDifference/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int DiagonalDifference(List<List<int>> arr)
+    {
+        int f = 0;
+        int s = 0;
+
+        for(int i = 0; i< arr.Count; i++) {
+            f += arr[i][i];
+            s += arr[i][arr.Count - i - 1];
+        }
+
+        return Math.Abs(f - s);
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt");
+        TextWriter textWriter = new StreamWriter(outputPath, true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.DiagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
