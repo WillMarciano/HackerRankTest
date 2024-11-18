891953735063af965ccf8395d0f30887928e9efd
WillMarciano <willian_marciano@hotmail.com>
2024-11-18 15:47:03 -0300

    Add Time Conversion

---
+++ Warmup/TimeConversion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string TimeConversion(string s) => Convert.ToDateTime(s).ToString("HH:mm:ss");
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt");
+        TextWriter textWriter = new StreamWriter(outputPath, true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.TimeConversion(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+
+        //Console.WriteLine(result);
+        //Console.ReadKey();
+    }
+}
