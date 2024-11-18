8c0c8dbc0f6c2077e1ae667114b36f1a5741274e
WillMarciano <willian_marciano@hotmail.com>
2024-11-18 14:56:24 -0300

    Add project files.

---
+++ Warmup/MiniMaxSum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void MiniMaxSum(List<int> arr)
+    {
+        arr.Sort();
+
+        long minimum = arr.Take(4).Sum(x => (long)x);
+        long maximum = arr.Skip(1).Sum(x => (long)x) ;
+
+        Console.WriteLine($"{ minimum } { maximum }");
+        Console.ReadKey();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.MiniMaxSum(arr);
+    }
+}
