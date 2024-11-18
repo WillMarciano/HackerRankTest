a505ad61756c52a1698a3f684f29daea518a289f
WillMarciano <willian_marciano@hotmail.com>
2024-11-18 16:27:58 -0300

    Add A Very Big Sum

---
+++ Warmup/AVeryBigSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'aVeryBigSum' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+     */
+
+    public static long AVeryBigSum(List<long> ar) => ar.Sum();
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        string outputPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt");
+        TextWriter textWriter = new StreamWriter(outputPath, true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<long> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList();
+
+        long result = Result.AVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
